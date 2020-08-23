c294c81c2bc42737ffe8004d8a79e865aa5ab479
danselmorgan120 <69982931+danselmorgan120@users.noreply.github.com>
2020-08-23 23:41:28 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -204,7 +204,7 @@ const second = (req, res) => {
 	res.cookie('cookieName', randomNumber);
 	//console.log('Cookies:', req.cookies); 
 	console.log("Cookies Successful");
-	res.sendFile(path.join(__dirname + '/index.html')); 
+	res.end(path.join(__dirname + '/index.html')); 
 
 }; 
 	
