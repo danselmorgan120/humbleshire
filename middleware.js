12e64afd106e71613b19cc67901286526f9ca511
danselmorgan120 <69982931+danselmorgan120@users.noreply.github.com>
2020-08-24 06:38:02 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -204,7 +204,7 @@ const second = (req, res) => {
 	res.cookie('cookieName', randomNumber);
 	//console.log('Cookies:', req.cookies); 
 	console.log("Cookies Successful");
-	res.send('<p2>Test Quincy (Gifted/Space) !!!<p2>');
+	res.send('<b>Test Quincy (Gifted/Space) !!!<b>');
 	
 	
 	
